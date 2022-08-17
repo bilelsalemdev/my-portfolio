762462c4f8ed1abb4454c972f146fb03cd1e7966
BILEL SALEM <70206023+bilelsalem2020@users.noreply.github.com>
2022-08-17 12:57:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ const ul=document.querySelector('.links ul');
 const icon= document.querySelector('.links .icon')
 const spans=document.querySelectorAll('.links .icon span')
 let clicked=false;
-function handleClick(){
-    ul.style.display ='block' ;
-    spans[1].style.display='none';
-    spans[0].style.transform = 'rotate(45deg)';
-    spans[2].style.transform='rotate(-45deg)'
-    clicked = true;
-    
-}
-function doNotHandleClick(){
-    ul.style.display='none'
-    spans[1].style.display='block';
-    spans[0].style.transform = 'rotate(0deg)';
-    spans[2].style.transform='rotate(0deg)'
-    clicked = false;
 
+
+
+function handleClick(){
+    if(!clicked){
+        spans[1].classList.add('clicked')
+        spans[0].classList.add('rotate-right')
+        spans[2].classList.add('rotate-left')
+        clicked = true
+        ul.style.display='block'
+    }else{
+        spans[1].classList.remove('clicked')
+        spans[0].classList.remove('rotate-right')
+        spans[2].classList.remove('rotate-left')
+        ul.style.display='none'
+        clicked = false
+        
+    }
+   
 }
 const text="My name is Bilel - Front-End Devoloper";
 let i=0;
